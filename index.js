1386095fab82f970cf03150038e94025bfee528a
ayush02303 <badolaayush295@gmail.com>
2025-11-17 14:55:41 +0530

    node initial

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express() 
+const path = require('path')
+
+app.set( "view engine" , "ejs")
+app.set("views" , path.join(__dirname, "/views"))
+
+app.get( '/' , (req, res)=> { res.render("home.ejs")
+
+})
+app.get( "/random", (req, res) => {
+    const num = Math.floor( Math.random()*6)+1; 
+    res.render('random', {rand : num})
+} )
+
+app.get( "/cats" , (req, res ) => {
+    const cats = ['one' , 'two' , 'seven', ]
+    res.render( "cats" , {cats})
+})
+
+
+
+app.listen( 3000 , ()=>{ console.log("Listening to port 30000 , Yes!")})
